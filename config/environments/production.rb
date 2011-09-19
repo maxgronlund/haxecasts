0c8781f7c55c6f729b499419bca6241ee174ce44
Jørgen Orehøj Erichsen <joe@erichsen.net>
2011-09-19 13:20:31 +0200

    Use digest

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,7 @@ HaXeCasts::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
+  config.assets.digest = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
